16e4a668db5087709e8dd216a9af4ff6c8a34136
Frantisek B <frantisekbabinsky.cz@gmail.com>
2026-01-25 15:13:12 +0100

    Clean the default project and create the front-end folder structure

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center text-white bg-black">
+      <h1 className="text-3xl font-bold">
+        Soundtrack Mood Explorer
+      </h1>
+    </main>
+  );
+}
